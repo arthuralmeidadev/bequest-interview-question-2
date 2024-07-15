fdb27a7f78e7455d16d2b4a5c9668111f17f963f
Arthur Almeida <arthuralmeida.office.dev@gmail.com>
2024-07-15 00:02:15 -0300

    feat/data integrity verification function

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,21 @@
 import React, { useEffect, useState } from "react";
+import { KJUR } from 'jsrsasign';
 
 const API_URL = "http://localhost:8080";
 
+const PUBLIC_KEY = `-----BEGIN PUBLIC KEY-----
+MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAo7RM10IA+G/Lugp+qnVA
+XXXnwexY3Djo40YZleGARDCfTeJqZTglORG84kU3SV8w9CQRwm6Wh8IVLpkbBqwQ
+XPjU56JNXNp1qwSIIzuSFB3cIp6iEHJ+ddKVlC4xtULOaY7BvekNKzhOLY1dWA3V
+odOOwetn9gqu8WYcY83MPeYlaCNvQJA644kxL0ybMWnYqJugxOntB4zr8VIFKnBa
+or8+pdCj12DTAHv3WxJ2wdNnvBLWXH1temK8Xzjt/0ROeJ5eQq+veHp/+0YevPv9
+Pr5VIF6deZPfJQ+O7BY/SP4aTMIE3IEXLPuJAwfkcotMdOqR5mtSDCSsm9czOzzL
+2wIDAQAB
+-----END PUBLIC KEY-----`;
+
 function App() {
   const [data, setData] = useState<string>();
+  const [signature, setSignature] = useState<string>();
 
   useEffect(() => {
     getData();
@@ -11,8 +23,9 @@ function App() {
 
   const getData = async () => {
     const response = await fetch(API_URL);
-    const { data } = await response.json();
+    const { data, signature } = await response.json();
     setData(data);
+    setSignature(signature);
   };
 
   const updateData = async () => {
@@ -28,8 +41,26 @@ function App() {
     await getData();
   };
 
+  /* Use KJUR's crypto 'Signature' class (from jsrsasign) to verify the signature
+     done with the RSA key pair using the public RSA key.
+     This is a straightforward method that ensures the data received
+     by the user is, in fact, not been tampered.
+     
+     I used the simple 'window.alert()' function to represent the integrity states
+  */
   const verifyData = async () => {
-    throw new Error("Not implemented");
+    if (data && signature) {
+      try {
+        const sig = new KJUR.crypto.Signature({ alg: "SHA256withRSA" });
+        sig.init(PUBLIC_KEY);
+        sig.updateString(data);
+        const isValid = sig.verify(signature);
+
+        window.alert(isValid ? "Valid data" : "Invalid data");
+      } catch (error) {
+        console.log(error);
+      }
+    }
   };
 
   return (
@@ -61,6 +92,14 @@ function App() {
         </button>
         <button style={{ fontSize: "20px" }} onClick={verifyData}>
           Verify Data
+        </button>
+
+        {/* 
+          This button is used to retrieve the original data,
+          in case the integrity of the data shown to the client has been affected
+        */}
+        <button style={{ fontSize: "20px" }} onClick={async () => await getData()}>
+          Recover Data
         </button>
       </div>
     </div>
--- server/app.ts
+++ server/app.ts
@@ -1,9 +1,26 @@
 import express from "express";
 import cors from "cors";
+import { createSign } from "crypto";
+import * as dotenv from "dotenv";
+dotenv.config();
 
 const PORT = 8080;
 const app = express();
 const database = { data: "Hello World" };
+const rsaPrivateKey = process.env.RSA_PRIVATE_KEY!;
+
+type SignedData = string;
+
+/* This function creates the signature that is used by the client
+   to ensure data integrity using the function 'createSign' from the crypto standard module
+*/
+function signData(data: string): SignedData {
+  const sign = createSign("SHA256");
+  sign.update(data);
+  sign.end();
+
+  return sign.sign(rsaPrivateKey, "hex");
+}
 
 app.use(cors());
 app.use(express.json());
@@ -11,7 +28,13 @@ app.use(express.json());
 // Routes
 
 app.get("/", (req, res) => {
-  res.json(database);
+  /*
+    Now there's also a signature for the data in the payload
+  */
+  res.json({
+    data: database.data,
+    signature: signData(database.data),
+  });
 });
 
 app.post("/", (req, res) => {
